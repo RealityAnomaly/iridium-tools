eeaf9524081d92a9ed1d79fa76c4baaf4a488d04
Alex Zero <me@alex0.net>
2024-12-13 07:11:52 +0000

    add more docs

--- src/core/main.ts
+++ src/core/main.ts
@@ -4,6 +4,9 @@ import * as types from '../sdk/types.ts';
 import { DataCallOptions, SDKError, SDKTaskResult, SDKTaskType } from "./types.ts";
 import { SDKStatus, SDKStatusMapper } from "./status.ts";
 
+/**
+ * Options passed to the SOAP API
+ */
 export interface IridiumGoOptions {
   username: string;
   password: string;
@@ -25,6 +28,9 @@ export class IridiumGo {
     this.client = client;
   };
 
+  /**
+   * Creates a new API client
+   */
   public static async create(options: IridiumGoOptions, logger?: winston.Logger): Promise<IridiumGo> {
     const client = await sdk.createClientAsync(`http://${options.server}/sdk/sdk.wsdl`, undefined, `http://${options.server}/sdk/sdk.php`);
     return new IridiumGo(options, logger, client);
@@ -199,6 +205,9 @@ export class IridiumGo {
   };
 
   // bare wrappers around SOAP methods
+  /**
+   * Adds or modifies an API user
+   */
   public async addModifyUser(user: types.sdk_user_t): Promise<void> {
     const [result] = await this.client.addModifyUserAsync({
       userCredentials: this.userCredentials,
@@ -208,6 +217,9 @@ export class IridiumGo {
     this.assert(result.addModifyUserResponse);
   };
 
+  /**
+   * Adds a new contact for the current user
+   */
   public async addNewContact(name: string, number: string, isFavourite: boolean = false): Promise<void> {
     const [result] = await this.client.addNewContactAsync({
       userCredentials: this.userCredentials,
@@ -219,6 +231,9 @@ export class IridiumGo {
     this.assert(result.addContactResult);
   };
 
+  /**
+   * Lists contacts for the current user
+   */
   public async getContactList(): Promise<types.sdk_contact_t[]> {
     const [result] = await this.client.getContactListAsync({
       userCredentials: this.userCredentials
@@ -227,6 +242,9 @@ export class IridiumGo {
     return this.assert(result.contactList).contacts!;
   };
 
+  /**
+   * Gets the device settings
+   */
   public async getSettings(reqList?: types.sdk_configuration_tagRequest_t[]): Promise<types.sdk_configuration_t[]> {
     const [result] = await this.client.getSettingsAsync({
       userCredentials: this.userCredentials,
@@ -236,6 +254,9 @@ export class IridiumGo {
     return this.assert(result.configs).configurations!;
   };
 
+  /**
+   * Gets a section of device configuration
+   */
   public async getStatus(options?: types.sdk_generic_option_request_t[]): Promise<SDKStatus> {
     const [result] = await this.client.getStatusAsync({
       userCredentials: this.userCredentials,
@@ -246,11 +267,17 @@ export class IridiumGo {
     return SDKStatusMapper.parse(sdkTypes);
   };
 
+  /**
+   * Returns information about the Iridium satellite transciever
+   */
   public async getTransceiverInfo(): Promise<types.sdk_status_t[]> {
     const [result] = await this.client.getTransceiverInfoAsync(null);
     return this.assert(result.transceiverInfo).status!;
   };
 
+  /**
+   * Returns information about the current user
+   */
   public async getUserDetails(): Promise<types.sdk_user_t> {
     const [result] = await this.client.getUserDetailsAsync({
       userCredentials: this.userCredentials
@@ -259,6 +286,9 @@ export class IridiumGo {
     return this.assert(result.login).user!;
   };
 
+  /**
+   * Lists users on the device
+   */
   public async getUserList(): Promise<types.sdk_user_t[]> {
     const [result] = await this.client.getUserListAsync({
       userCredentials: this.userCredentials
@@ -267,11 +297,17 @@ export class IridiumGo {
     return this.assert(result.getUserListResponse).users!;
   };
 
+  /**
+   * Returns the API and firmware versions
+   */
   public async getVersion(): Promise<types.sdk_version_t> {
     const [result] = await this.client.getVersionAsync(null);
     return result.versionNumber;
   };
 
+  /**
+   * Updates a contact for the current user
+   */
   public async modifyContact(contact: types.sdk_contact_t): Promise<void> {
     const [result] = await this.client.modifyContactAsync({
       userCredentials: this.userCredentials,
@@ -281,6 +317,9 @@ export class IridiumGo {
     this.assert(result.modifyContactResult);
   };
 
+  /**
+   * Performs a list of SDK requests
+   */
   public async performTasks(requestList: types.sdk_task_request_t[]): Promise<SDKTaskResult[]> {
     const [result] = await this.client.performTaskAsync({
       userCredentials: this.userCredentials,
@@ -301,6 +340,9 @@ export class IridiumGo {
     });
   };
 
+  /**
+   * Performs a singular SDK request and returns the result
+   */
   public async performTask(taskID: SDKTaskType, options?: types.sdk_generic_option_t[]): Promise<SDKTaskResult> {
     const results = await this.performTasks([{
       taskID: taskID.toString(),
@@ -310,6 +352,9 @@ export class IridiumGo {
     return results.find(r => r.taskID === taskID)!;
   };
 
+  /**
+   * Deletes a contact for the current user
+   */
   public async removeContact(contactID: number): Promise<void> {
     const [result] = await this.client.removeContactAsync({
       userCredentials: this.userCredentials,
@@ -319,6 +364,9 @@ export class IridiumGo {
     this.assert(result.removeContactResult);
   };
 
+  /**
+   * Deletes a user based on its username
+   */
   public async removeUser(userName: string): Promise<void> {
     const [result] = await this.client.removeUserAsync({
       userCredentials: this.userCredentials,
@@ -328,6 +376,9 @@ export class IridiumGo {
     this.assert(result.removeUserResponse);
   };
 
+  /**
+   * Updates the device configuration
+   */
   public async setSettings(setList: types.sdk_configuration_setRequest_list_t): Promise<types.sdk_configuration_set_Result_t[]> {
     const [result] = await this.client.setSettingsAsync({
       userCredentials: this.userCredentials,
